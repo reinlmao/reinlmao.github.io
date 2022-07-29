515db1c67f8cc9ac2cad2dc752be9a5784ed486c
reinlmao <rekreingd@gmail.com>
2022-07-29 00:51:48 -0500

    Update pixi-test.js

--- src/scripts/pixi-test.js
+++ src/scripts/pixi-test.js
@@ -1,7 +1,7 @@
 var app = new PIXI.Application(640, 360);
 document.body.appendChild(app.view);
 
-var iterations = 21.0;
+var iterations = 11;
 var length = 500.0;
 
 for (var i = 0; i < iterations; i++) {
@@ -10,11 +10,11 @@ for (var i = 0; i < iterations; i++) {
     circle.beginFill(0x5cafe2);
     circle.drawCircle(0, 0, 2);
     circle.x = 60 + (increment);
-    circle.y = 60 + (Math.sin(increment) * 30);
+    circle.y = 60 + (Math.sin(increment) * 10);
     app.stage.addChild(circle);
 }
 
-var second_iterations = 20
+var second_iterations = 100
 
 for (var i = 0; i < second_iterations; i++) {
     var increment = i * (length / second_iterations);
@@ -22,6 +22,6 @@ for (var i = 0; i < second_iterations; i++) {
     circle.beginFill(0x5cafe2);
     circle.drawCircle(0, 0, 2);
     circle.x = 60 + (increment);
-    circle.y = 300 + (Math.sin(increment) * 30);
+    circle.y = 300 + (Math.sin(increment) * 10);
     app.stage.addChild(circle);
 }
